00e829cfb3782f650b29292a8d4a1ad0368707df
maLu70 <166057576+maLu70@users.noreply.github.com>
2025-09-08 19:08:51 -0300

    Add files via upload

---
+++ Sir.Vine/src/test/java/com/ifsp/Sir/Vine/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.Sir.Vine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
